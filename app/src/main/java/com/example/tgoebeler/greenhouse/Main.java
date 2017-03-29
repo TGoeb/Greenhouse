dcf770be3c04b7aa7112cdac4b25d740b22478c4
tgoebeler <stickeykeys0@gmail.com>
2017-03-29 08:40:35 -0400

    v0..0.1

--- app/src/main/java/com/example/tgoebeler/greenhouse/Main.java
+++ app/src/main/java/com/example/tgoebeler/greenhouse/Main.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class Main extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class Main extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.i(TAG, "onCreate");
     }
 
     @Override
